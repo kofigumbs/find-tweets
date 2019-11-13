a0cdbdeb5a2b3c43e8af8f65a86b11fe46f2e7aa
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-11-13 12:21:41 -0500

    Twitter auth

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,17 @@
 <script>
-  fetch("/login")
+  const token = new URLSearchParams(window.location.search).get("oauth_token");
+
+  function login() {
+    fetch("/login")
+      .then(res => res.text())
+      .then(form => form.match(/oauth_token=([\w-]+)/)[1])
+      .then(token => window.location = `https://api.twitter.com/oauth/authenticate?oauth_token=${token}`)
+  }
 </script>
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Find Tweets</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<h1>Token: {token}</h1>
+<button on:click={login}>Login</button>
