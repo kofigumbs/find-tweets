d4e388a177c1631704b5252ce14d7849328e06b0
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-11-13 19:45:33 -0500

    Use twitter js embed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,33 +1,43 @@
+<style>
+  :global(twitter-widget) {
+    margin: 0 auto;
+  }
+</style>
 <script>
   // TODO
-  //  1. Click through (using Twitter embed)
-  //  2. Make Twitter do the hard work for user_timeline search
-  //  3. How to show DMs?
+  //  1. Make Twitter do the hard work for user_timeline search
+  //  2. How to show DMs?
 
   let data = [];
   let term = "";
 
-  function withStatusUrls(tweets) {
-    return tweets.map(x => ({ ...x, url: "https://twitter.com/i/web/status/" + x.id_str }));
+  $: console.log(data);
+
+  function embed(node, tweet) {
+    twttr.widgets.createTweet(tweet.id_str, node);
+    return {
+      update(newTweet) {
+        node.innerHTML = "";
+        twttr.widgets.createTweet(newTweet.id_str, node);
+      }
+    };
   }
 
   function getTweets() {
     Promise.all([
       fetch("/api/statuses/user_timeline.json?count=200")
-      .then(response => response.json())
-      .then(withStatusUrls),
+      .then(response => response.json()),
 
       fetch("/api/favorites/list.json?count=200")
-      .then(response => response.json())
-      .then(withStatusUrls),
+      .then(response => response.json()),
 
-      fetch("/api/direct_messages/events/list.json?count=200")
-      .then(response => response.json())
-      .then(json => json.events.map(event => event.message_create.message_data)),
+      // fetch("/api/direct_messages/events/list.json?count=50")
+      // .then(response => response.json())
+      // .then(json => json.events.map(event => event.message_create.message_data)),
 
-    ]).then(json => json.flat())
-      .then(json => json.filter(tweet => tweet.text.match(new RegExp(term, "i"))))
-      .then(json => data = json.map(tweet => ({ text: tweet.text, url: tweet.url })));
+    ]).then(json => {
+      data = json.flat().filter(tweet => tweet.text.match(new RegExp(term, "i")));
+    });
   }
 </script>
 
@@ -41,4 +51,4 @@
   <input bind:value={term}/>
   <button type="submit">Get Tweets</button>
 </form>
-{#each data as { text, url }}<p><a href="{url}" target="_blank">{text}</a></p>{/each}
+{#each data as tweet }<div use:embed={tweet}></div>{/each}
