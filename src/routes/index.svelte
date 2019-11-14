fb6a3b755d76ca9edc76c84942082f24359051a5
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-11-13 19:50:17 -0500

    Move checklist into README

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,14 +4,8 @@
   }
 </style>
 <script>
-  // TODO
-  //  1. Make Twitter do the hard work for user_timeline search
-  //  2. How to show DMs?
-
   let data = [];
   let term = "";
-
-  $: console.log(data);
 
   function embed(node, tweet) {
     twttr.widgets.createTweet(tweet.id_str, node);
@@ -31,9 +25,9 @@
       fetch("/api/favorites/list.json?count=200")
       .then(response => response.json()),
 
-      // fetch("/api/direct_messages/events/list.json?count=50")
-      // .then(response => response.json())
-      // .then(json => json.events.map(event => event.message_create.message_data)),
+      fetch("/api/direct_messages/events/list.json?count=50")
+      .then(response => response.json())
+      .then(json => json.events.map(event => event.message_create.message_data)),
 
     ]).then(json => {
       data = json.flat().filter(tweet => tweet.text.match(new RegExp(term, "i")));
