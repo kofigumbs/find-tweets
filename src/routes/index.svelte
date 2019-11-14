9b97b80b866cfdaf539316c101e2096489b69c44
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-11-13 20:14:35 -0500

    More search params

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,10 +19,10 @@
 
   function getTweets() {
     Promise.all([
-      fetch("/api/statuses/user_timeline.json?count=200")
+      fetch("/api/statuses/user_timeline.json?count=200&trim_user=1&include_rts=1")
       .then(response => response.json()),
 
-      fetch("/api/favorites/list.json?count=200")
+      fetch("/api/favorites/list.json?count=200&include_entities=false")
       .then(response => response.json()),
 
       fetch("/api/direct_messages/events/list.json?count=50")
